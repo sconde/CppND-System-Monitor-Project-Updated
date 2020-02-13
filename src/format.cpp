a28104f7a5a97e43e87eb587c0a16e4616541e53
Sidafa Conde <sconde@sandia.gov>
2020-02-13 10:43:38 -0500

    ElapsedTime: format

--- src/format.cpp
+++ src/format.cpp
@@ -2,19 +2,20 @@
 
 #include "format.h"
 
-using std::string;
+inline std::string PrettyNumber(const int num){
+  if ( num < 10 )
+    return "0" + std::to_string(num);
+  return std::to_string(num);
+}
 
 // Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-string Format::ElapsedTime(long seconds ) {
-  const auto sec = (seconds % 60);
-  const auto hrs = (seconds / 3600);
-  const auto min = (seconds - hrs * 3600 - sec) / 60;
+std::string Format::ElapsedTime(long seconds ) {
 
-  std::string str_hrs = (hrs < 10 ? ("0" + std::to_string(hrs)) : (std::to_string(hrs)));
-  std::string str_min = (min < 10 ? ("0" + std::to_string(min)) : (std::to_string(min)));
-  std::string str_sec = (sec < 10 ? ("0" + std::to_string(sec)) : (std::to_string(sec)));
+  const int hours = seconds / 60 / 60;
+  const int secs = seconds % 60;
+  const int minutes = (seconds - hours * 3600 - secs) / 60;
 
-  return (str_hrs + ":" + str_min + ":" + str_sec);
+  return ( PrettyNumber(hours) + ":" + PrettyNumber(minutes) + ":" + PrettyNumber(secs) );
 }
